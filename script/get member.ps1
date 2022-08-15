421f789fd87a6a95bbd1dc1de173a1ae9d8696d2
prabhaka-tonigandla <74236994+prabhaka-tonigandla@users.noreply.github.com>
2022-08-15 17:53:29 +0530

    Get member and sort example

---
+++ script/get member.ps1
@@ -0,0 +1,14 @@
+﻿$sourcePath = "C:\Prabhakar\Compare\source\"
+Get-ChildItem | Get-Member
+
+Get-ChildItem -Path $sourcePath | Select-Object Name,FullName,lastwriteTime | sort -Property lastwriteTime -Descending
+
+
+
+
+
+
+
+
+
+
